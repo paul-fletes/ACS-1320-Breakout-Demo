f239eaa32f0163205fa946fc6e3142faf06c4891
Paul Fletes <paul.fletes@gmail.com>
2023-02-02 23:31:04 -0600

    refactor game objects with classes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 const canvas = document.getElementById('myCanvas'); // reference to canvas obj
 const ctx = canvas.getContext('2d'); // create context to paint canvas
 // declare ball radius for collision calcs
@@ -9,19 +10,8 @@ const color = '#0095DD';
 const paddleHeight = 10;
 const paddleWidth = 75;
 const paddleXStart = (canvas.width - paddleWidth) / 2;
-let paddleX = paddleXStart;
-// declare x/y coordinates of ball, and speed of ball with dx/dy
-// let x = canvas.width / 2;
-// let y = canvas.height - 30;
-// let dx = 2;
-// let dy = -2;
-let ball = {
-  x: 0,
-  y: 0,
-  dx: 0,
-  dy: 0,
-};
-resetBallAndPaddle();
+const paddleYStart = canvas.height - paddleHeight;
+// let paddleX = paddleXStart;
 // declare keypress variables
 let rightPressed = false;
 let leftPressed = false;
@@ -33,18 +23,145 @@ const brickHeight = 20;
 const brickPadding = 10;
 const brickOffsetTop = 30;
 const brickOffsetLeft = 30;
-// declare a score variable
-let score = 0;
-// declare a variable to track lives
-let lives = 3;
-// declare an array to hold brick field
-const bricks = [];
-for (let c = 0; c < brickColumnCount; c += 1) {
-  bricks[c] = [];
-  for (let r = 0; r < brickRowCount; r += 1) {
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
+
+class Ball {
+  constructor(x = 0, y = 0, dx = 2, dy = -2, radius = 10, color = 'red') {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  // define a fxn that draws a ball using the context methods
+  render(ctx) {
+    // draw a ball
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, PI2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
   }
 }
+
+class Brick {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.status = 1;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+// Bricks
+class Bricks {
+  constructor(cols, rows) {
+    this.cols = cols;
+    this.rows = rows;
+    this.bricks = [];
+    this.init();
+  }
+
+  init() {
+    for (let c = 0; c < this.cols; c += 1) {
+      this.bricks[c] = [];
+      for (let r = 0; r < this.rows; r += 1) {
+        const brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
+        this.bricks[c][r] = new Brick(brickX, brickY, brickWidth, brickHeight, color);
+      }
+    }
+  }
+
+  render(ctx) {
+    for (let c = 0; c < this.cols; c += 1) {
+      for (let r = 0; r < this.rows; r += 1) {
+        const brick = this.bricks[c][r];
+        if (brick.status === 1) {
+          brick.render(ctx);
+        }
+      }
+    }
+  }
+}
+
+const bricks = new Bricks(brickColumnCount, brickRowCount)
+// Paddle
+class Paddle {
+  constructor(x, y, width, height, color = 'red') {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  moveBy(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+
+  moveTo(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const paddle = new Paddle(paddleXStart, paddleYStart, paddleWidth, paddleHeight, color);
+
+// Score
+// Lives
+
+class GameLabel {
+  constructor(text, x, y, color, font = '16px Arial') {
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.value = 0;
+    this.font = font;
+  }
+
+  render(ctx) {
+    ctx.font = this.font;
+    ctx.fillStyle = this.color;
+    ctx.fillText(`${this.text} ${this.value}`, this.x, this.y);
+  }
+}
+
+const scoreLabel = new GameLabel('Score: ', 8, 20);
+const livesLabel = new GameLabel('Lives: ', canvas.width - 65, 20);
+livesLabel.value = 3;
+
+// Game - owns the draw and runs game loop
+
+let ball = new Ball(0, 0, 2, -2, ballRadius, color);
+
+resetBallAndPaddle();
 
 // define fxns that handle key down/up events
 function keyDownHandler(e) {
@@ -66,7 +183,7 @@ function keyUpHandler(e) {
 function mouseMoveHandler(e) {
   const relativeX = e.clientX - canvas.offsetLeft;
   if (relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth / 2;
+    paddle.moveTo(relativeX - paddle.width / 2, paddleYStart)
   }
 }
 
@@ -75,19 +192,11 @@ document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 document.addEventListener('mousemove', mouseMoveHandler, false);
 
-// // define a fxn that returns a random hex color value
-// function randColor() {
-//   return (
-//     `#${(Math.floor(Math.random() * 0x1000000) + 0x1000000)
-//       .toString(16).substring(1)}`
-//   );
-// }
-
 // define a fxn that detects ball/brick collision
 function collisionDetection() {
-  for (let c = 0; c < brickColumnCount; c += 1) {
-    for (let r = 0; r < brickRowCount; r += 1) {
-      const brick = bricks[c][r];
+  for (let c = 0; c < bricks.cols; c += 1) {
+    for (let r = 0; r < bricks.rows; r += 1) {
+      const brick = bricks.bricks[c][r];
       // change ball direction on ball/brick collision
       if (brick.status === 1) {
         if (ball.x > brick.x
@@ -96,9 +205,9 @@ function collisionDetection() {
           && ball.y < brick.y + brickHeight) {
           ball.dy = -ball.dy;
           brick.status = 0;
-          score += 1; // award point each hit
+          scoreLabel.value += 1; // award point each hit
           // game win logic
-          if (score === brickRowCount * brickColumnCount) {
+          if (scoreLabel.value === bricks.rows * bricks.cols) {
             alert(`YOU WIN, CONGRATS! TOTAL SCORE: ${score}!`);
             document.location.reload();
           }
@@ -114,12 +223,12 @@ function collisionsWithCanvasAndPaddle() {
   if (ball.y + ball.dy < ballRadius) {
     ball.dy = -ball.dy;
   } else if (ball.y + ball.dy > canvas.height - ballRadius) {
-    if (ball.x > paddleX && ball.x < paddleX + paddleWidth) {
+    if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
       ball.dy = -ball.dy * 1.1; // '* 1.1' increases dy by 10% each paddle hit
     } else { // game loss logic
-      lives -= 1;
-      if (!lives) {
-        alert(`GAME OVER! TOTAL SCORE: ${score}!`);
+      livesLabel.value -= 1;
+      if (livesLabel.value < 1) {
+        alert(`GAME OVER! TOTAL SCORE: ${scoreLabel.value}!`);
         document.location.reload();
       } else {
         resetBallAndPaddle();
@@ -138,73 +247,15 @@ function resetBallAndPaddle() {
   ball.y = canvas.height - 30;
   ball.dx = 2;
   ball.dy = -2;
-  paddleX = paddleXStart;
-}
-
-// define a function to update score
-function drawScore() {
-  ctx.font = '16px Arial';
-  ctx.fillStyle = color;
-  ctx.fillText(`Score: ${score}`, 8, 20);
-}
-
-// define a function to update lives
-function drawLives() {
-  ctx.font = '16px Arial';
-  ctx.fillStyle = color;
-  ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
-}
-
-// define a fxn that draws a ball using the context methods
-function drawBall() {
-  // draw a ball
-  ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ballRadius, 0, PI2);
-  ctx.fillStyle = color;
-  ctx.fill();
-  ctx.closePath();
-}
-
-// define a fxn that draws a paddle
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = color;
-  ctx.fill();
-  ctx.closePath();
-}
-
-// define a fxn that draws the brick field
-function drawBricks() {
-  for (let c = 0; c < brickColumnCount; c += 1) {
-    for (let r = 0; r < brickRowCount; r += 1) {
-      if (bricks[c][r].status === 1) {
-        const brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
-        const brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }
-}
-
-// define a function that updates x/y with dx/dy to move ball
-function moveBall() {
-  ball.x += ball.dx;
-  ball.y += ball.dy;
+  paddle.x = paddleXStart;
 }
 
 // define a function that updates paddle mvmts with right/left press
 function movePaddle() {
-  if (rightPressed) {
-    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
-  } else if (leftPressed) {
-    paddleX = Math.max(paddleX - 7, 0);
+  if (rightPressed && paddle.x < canvas.width - paddle.width) {
+    paddle.moveBy(7, 0);
+  } else if (leftPressed && paddle.x > 0) {
+    paddle.moveBy(-7, 0);
   }
 }
 
@@ -213,21 +264,21 @@ function draw() {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // call drawball fxn
-  drawBall();
+  ball.render(ctx);
   // call drawpaddle fxn
-  drawPaddle();
+  paddle.render(ctx);
   // call drawscore fxn
-  drawScore();
+  scoreLabel.render(ctx);
   // call collisiondetection fxn
   collisionDetection();
   // call canvas/paddle collision fxn
   collisionsWithCanvasAndPaddle();
   // call drawbricks fxn
-  drawBricks();
+  bricks.render(ctx);
   // call drawlives fxn
-  drawLives();
+  livesLabel.render(ctx);
   // call moveball fxn
-  moveBall();
+  ball.move();
   // call movepaddle fxn
   movePaddle();
   // redraw the screen
