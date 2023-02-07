eda2a0748fa3c66b97db975c2c39ff209d7ce675
Paul Fletes <paul.fletes@gmail.com>
2023-02-07 13:50:35 -0600

    remove Paddle.js

--- Paddle.js
+++
@@ -1,9 +0,0 @@
-import Sprite from './Sprite.js';
-
-class Paddle extends Sprite {
-  constructor(x, y, width, height, color = 'red') {
-    super(x, y, width, height, color);
-  }
-}
-
-export default Paddle;
